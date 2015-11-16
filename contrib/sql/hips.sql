8f89b4455dbec7ab833a94b7df3e44414724d213
theflakes <theflakes@gmail.com>
2015-11-15 22:27:21 -0500

    Add total bytes field

--- contrib/sql/hips.sql
+++ contrib/sql/hips.sql
@@ -9,6 +9,7 @@ INSERT IGNORE INTO fields_classes_map (class_id, field_id, field_order) VALUES (
 INSERT IGNORE INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="HIPS"), (SELECT id FROM fields WHERE field="dstip"), 7);
 INSERT IGNORE INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="HIPS"), (SELECT id FROM fields WHERE field="dstport"), 8);
 INSERT IGNORE INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="HIPS"), (SELECT id FROM fields WHERE field="proto"), 9);
+INSERT IGNORE INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="HIPS"), (SELECT id FROM fields WHERE field="total_bytes"), 10);
 INSERT IGNORE INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="HIPS"), (SELECT id FROM fields WHERE field="hostname"), 11);
 INSERT IGNORE INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="HIPS"), (SELECT id FROM fields WHERE field="user"), 12);
 INSERT IGNORE INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="HIPS"), (SELECT id FROM fields WHERE field="parent_image"), 13);
