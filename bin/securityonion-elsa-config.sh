5a3235ec5dc8c5ab9071c1d0213455037e6a2e43
doug <doug.burks@gmail.com>
2016-05-01 14:39:31 -0400

    Issue 898: ELSA: set master peer label to hostname instead of 127.0.0.1

--- bin/securityonion-elsa-config.sh
+++ bin/securityonion-elsa-config.sh
@@ -216,6 +216,8 @@ function config_webnode() {
 		#IP=`ifconfig | grep "inet addr" | awk '{print $2}' | cut -d\: -f2 | grep -v "127.0.0.1" | head -1`
 		sed -i "s/^.*\"pcap_url\": \"http\:\/\/streamdb\",.*$/\t\"pcap_url\": \"\/capme\",/" /etc/elsa_web.conf
 	fi   
+	HOSTNAME=`hostname`
+	sed -i "s|\"127.0.0.1\": {|\"$HOSTNAME\": {|g" /etc/elsa_web.conf
 	echo "* Beginning Web Install:" | $LOGGER
 	echo "* Configuring mysql schema" | $LOGGER
 	mysqladmin -uroot $MYSQL_PORT create elsa_web 
@@ -271,9 +273,6 @@ function config_webnode() {
 
 	echo "* Restarting cron" | $LOGGER
 	service cron restart
-
-	#echo "* Opening 3154/tcp.." | $LOGGER
-	#ufw allow 3154/tcp
 
 	echo "* Retrieving GeoIP City databases..."	 | $LOGGER
 	mkdir -p /usr/local/share/GeoIP 
