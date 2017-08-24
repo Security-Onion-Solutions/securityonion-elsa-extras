7736a2c20962851d0ab9c49d2d8a878ff0c99f69
doug <doug.burks@gmail.com>
2017-08-24 10:26:20 -0400

    securityonion-elsa-extras: cron.pl should check to see if it's already running #1059

--- contrib/securityonion-elsa-cron.sh
+++ contrib/securityonion-elsa-cron.sh
@@ -2,4 +2,12 @@
 
 eval $(perl -Mlocal::lib=/opt/elsa/perl5)
 
+# check to see if we're already running
+for pid in $(pidof -x securityonion-elsa-cron.sh); do
+    if [ $pid != $$ ]; then
+        echo_error_msg 0 "Process is already running with PID $pid"
+        exit 1
+    fi
+done
+
 perl /opt/elsa/web/cron.pl -c /etc/elsa_web.conf
