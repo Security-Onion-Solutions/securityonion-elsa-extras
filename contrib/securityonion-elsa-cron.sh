ab2844664b8a9b9454ecfd7ca88436fb4a023aec
doug <doug.burks@gmail.com>
2017-08-24 14:49:45 -0400

    improve cron job

--- contrib/securityonion-elsa-cron.sh
+++ contrib/securityonion-elsa-cron.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-eval $(perl -Mlocal::lib=/opt/elsa/perl5)
+LOG="/nsm/elsa/data/elsa/log/cron.log"
 
-# check to see if we're already running
-for pid in $(pidof -x securityonion-elsa-cron.sh); do
-    if [ $pid != $$ ]; then
-        echo_error_msg 0 "Process is already running with PID $pid"
-        exit 1
-    fi
-done
+# This cron job runs every minute.
+# Under normal circumstances, it will complete in under 10 seconds.
+# If it takes longer than 60 seconds, we need to prevent multiple instances.
+if ps aux | grep -q "perl /opt/elsa/web/cron.pl -c /etc/elsa_web.con[f]"; then
+	echo "/opt/elsa/web/cron.pl is already running:" >> $LOG
+	ps aux | grep "perl /opt/elsa/web/cron.pl -c /etc/elsa_web.con[f]" >> $LOG
+	exit
+fi
+
+eval $(perl -Mlocal::lib=/opt/elsa/perl5)
 
 perl /opt/elsa/web/cron.pl -c /etc/elsa_web.conf
